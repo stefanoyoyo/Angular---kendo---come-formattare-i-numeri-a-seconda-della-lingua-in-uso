990167491d02267dcd35a771a6efe050136cb1e5
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2023-01-26 10:54:29 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { IntlService } from '@progress/kendo-angular-intl';
 
 @Component({
-    selector: 'my-app',
-    template: `
+  selector: 'my-app',
+  template: `
     <div class="col-xs-12 col-md-12" style="margin-bottom: 1em;">
         <div class="col-xs-12 col-md-9">
             <h5 style="color:red;">Format 1234.567 using a 'n2' format</h5>
@@ -31,22 +31,22 @@ import { IntlService } from '@progress/kendo-angular-intl';
         </div>
     </div>
 
-    <div class="col-xs-12 col-md-12">
+    <div class="col-xs-12 col-md-12" style="margin-bottom: 1em;">
         <div class="col-xs-12 col-md-9">
-            <h5 style="color:red;">Format 1234.567 using a customr'##,#.00' format</h5>
+            <h5 style="color:red;">Format 1234.567 using ITALIAN</h5>
         </div>
         <div class="col-xs-12 col-md-9" style="font-size: 1.2em;">
-            Result: <strong><em>{{ intl.formatNumber(2345678, "##,#.00") }}</em></strong>
+            Result: <strong><em>{{ intl.formatNumber(1234.567, customCurrencyOptions, "de") }}</em></strong>
         </div>
     </div>
-    `
+    `,
 })
 export class AppComponent {
-    public customCurrencyOptions = {
-        style: 'currency',
-        currency: 'USD',
-        currencyDisplay: 'displayName'
-    };
+  public customCurrencyOptions = {
+    style: 'currency',
+    currency: 'USD',
+    currencyDisplay: 'displayName',
+  };
 
-    constructor(public intl: IntlService) {}
+  constructor(public intl: IntlService) {}
 }
